4a453e45652f32149d70956309fb51703d644a2e
wcgregory <129503132+wcgregory@users.noreply.github.com>
2024-06-12 10:04:06 +0000

    exercise 1a completed

--- exercises/1a.js
+++ exercises/1a.js
@@ -13,4 +13,22 @@
  */
 export function addEven(n) {
   // TODO
+  // let indx = 1
+  let total = 0
+
+  for (let indx = 0; indx <= n; indx += 2) {
+    total += indx
+  }
+
+  // while (indx <= n) {
+  //   indx++
+  //  if (indx % 2 == 0) {
+  //    total += indx
+  //  }
+  //}
+  
+  return total
 }
+
+const whatis = addEven(11)
+console.log(whatis)
