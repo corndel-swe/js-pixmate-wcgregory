6ad27c1deac5a14845bc99341164abd5579ad29b
wcgregory <129503132+wcgregory@users.noreply.github.com>
2024-06-14 15:05:47 +0000

    new loading animation message

---
+++ animations/ani_loading.js
@@ -0,0 +1,21 @@
+import animate from './animate.js'
+import chalk from 'chalk'
+
+export function loading(length) {
+  const size = length
+  const frames = Array()
+
+  for (let i = 0; i <= length; i++) {
+    let percentage = ((i / length) * 100)
+    let line = chalk.white('|')
+    line += chalk.green('#').repeat(i)
+    line += ' '.repeat(length - i)
+    line += chalk.white('|')
+    line += ` ${percentage}%`
+
+    frames.push(line)
+  }
+  animate(frames, 0.5)
+}
+
+loading(10)
