acaf63167bbf8d3c715f0b29d136cd1b25deeb92
wcgregory <129503132+wcgregory@users.noreply.github.com>
2024-06-17 16:55:02 +0000

    enhancement to checkerboard with defaultWidth parameter

--- drawings/checkerboard.js
+++ drawings/checkerboard.js
@@ -2,21 +2,31 @@
 import chalk from 'chalk'
 
 /**
- * Create a checkerboard function that accept a size parameter
+ * Create a checkerboard function that accepts a size parameter
  * Draw a checkerboard shape to the console based on size
+ * @param length, the length of the board
+ * @param defaultWidth boolean
+ *  if true keeps the number of stars at 5 or 4
+ *  if false is based on the length
  */
 
-export function checkerBoard(size) {
-  // use size to build the side triangle
+function checkerBoard(length, defaultWidth) {
+  // length, is to build the length of the board
+  // defaultWidth, true keeps the default number of stars, false is based on length
   let line = ''
-  for (let row = 1; row <= size; row++) {
-    if (row % 2 !== 0) {
-      line = chalk.red('* ').repeat(size)
-    } else {
-      line = chalk.red(' *').repeat(size - 1)
-    }
+
+  for (let row = 1; row <= length; row++) {
+    if (row % 2 !== 0 && !defaultWidth) {
+      line = chalk.red('* ').repeat(length - 1) + chalk.red('*')
+    } else if (row % 2 !== 0 && defaultWidth) {
+      line = chalk.red('* ').repeat(4) + chalk.red('*')
+    } else if (row % 2 === 0 && !defaultWidth) {
+      line = chalk.red(' *').repeat(length - 1)
+    } else line = chalk.red(' *').repeat(4)
     console.log(line)
   }
 }
 
-checkerBoard(8)
+// checkerBoard(6)
+checkerBoard(6, true)
+// checkerBoard(6, false)
